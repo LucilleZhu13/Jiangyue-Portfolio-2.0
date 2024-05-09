426eeff796c9ddb6f54a04ec3050e173caf8d40e
Jiangyue Zhu <jzhu484@gatech.edu>
2024-05-09 10:24:34 -0400

    changed buttons

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function toSkills() {
     window.location.hash = "#skill_title";
 }
+
+var tabs = document.getElementsByClassName('tab-head')[0].getElementsByTagName('h2'),
+                contents = document.getElementsByClassName('tab-content')[0].getElementsByTagName('div');
+
+            (function changeTab(tab) {
+                for(var i = 0, len = tabs.length; i < len; i++) {
+                    tabs[i].onmouseover = showTab;
+                }
+            })();
+
+            function showTab() {
+                for(var i = 0, len = tabs.length; i < len; i++) {
+                    if(tabs[i] === this) {
+                        tabs[i].className = 'selected';
+                        contents[i].className = 'show';
+                    } else {
+                        tabs[i].className = '';
+                        contents[i].className = '';
+                    }
+                }
+            }
